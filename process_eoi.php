359f5caf9c0394e53002cb2381adc32fdfcb962a
rainsake <105924752@student.swin.edu.au>
2025-05-22 18:08:22 +1000

    added validation for fields

--- process_eoi.php
+++ process_eoi.php
@@ -6,19 +6,19 @@ echo '<pre>';
 print_r($_POST);
 echo '</pre>';
 
-
+// DEFINING FIELDS //
 $refNum = trim($_POST['refNum']);
 $firstName = trim($_POST['firstName']);
 $lastName = trim($_POST['lastName']);
 
-$inputDate = trim($_POST['dateOfBirth']);
-$convertedDate = DateTime::createFromFormat('d/m/Y', $inputDate);
+// converting date into the correct format for mySQL
+$inputDate = trim($_POST['dateOfBirth']); //gets input date
+$convertedDate = DateTime::createFromFormat('d/m/Y', $inputDate); //creates a format using the input
 if ($convertedDate) {
-    $dob = $convertedDate->format('Y-m-d');
+    $dob = $convertedDate->format('Y-m-d'); //converts into the correct format
 } else {
     $dob = null;
 }
-// $dob = isset($_POST['dob']) ?? '';
 
 $gender = $_POST['gender'] ?? '';
 
@@ -26,8 +26,10 @@ $address = trim($_POST['address']);
 $suburb = trim($_POST['suburb']);
 $state = trim($_POST['state']);
 $zip = trim($_POST['zip']);
+
 $email = trim($_POST['email']);
 $phone = trim($_POST['phone']);
+
 
 $skills = isset($_POST['requiredTechnical']) ? $_POST['requiredTechnical'] : [];
 $skillsCPP   = in_array('cpp', $skills) ? 1 : 0;
@@ -37,14 +39,45 @@ $skillsJS    = in_array('js', $skills) ? 1 : 0;
 $skillsMySQL = in_array('mysql', $skills) ? 1 : 0;
 $skillsPS    = in_array('ps', $skills) ? 1 : 0;
 $skillsGA    = in_array('ga', $skills) ? 1 : 0;
-// $skillsCPP = isset($_POST['skillsCPP']) ? 1 : 0;
-// $skillsHTML = isset($_POST['skillsHTML']) ? 1 : 0;
-// $skillsCSS = isset($_POST['skillsCPP']) ? 1 : 0;
-// $skillsJS = isset($_POST['skillsJS']) ? 1 : 0;
-// $skillsMySQL = isset($_POST['skillsMySQL']) ? 1 : 0;
-// $skillsPS = isset($_POST['skillsPS']) ? 1 : 0;
-// $skillsGA = isset($_POST['skillsGA']) ? 1 : 0;
+
 $otherSkills = trim($_POST['otherSkills']);
+
+// server-side validation //
+if (!preg_match('/^[a-zA-Z]{1,20}$/', $firstName) || !preg_match('/^[a-zA-Z]{1,20}$/', $lastName)) {
+    die("Both first and last names should be less than 20 characters long, and should only contain letters."); 
+} // checks length and type of firstname/lastname
+
+if (!preg_match('/^[a-zA-Z\s\-]{1,40}$/', $address) || !preg_match('/^[a-zA-Z\s\-]{1,20}$/', $suburb)) {
+    die("Street Address and Suburb/towns may only be at maximum 40 characters long.");
+} // checks the length of the address and suburb. Allows spaces and hyphens
+
+if (!preg_match('/^\d{4}$/', $zip)) {
+    die("PostCode must be 4 numbers long.");
+} // checks the length of postcode
+
+$stateZips = [ // regex rules for each state's postcodes
+    'NSW' => '/^(1|2)\d{3}$/',
+    'ACT' => '/^(0|2)\d{3}$/',
+    'VIC' => '/^(3|8)\d{3}$/',
+    'QLD' => '/^(4|9)\d{3}$/',
+    'SA'  => '/^5\d{3}$/',
+    'WA'  => '/^6\d{3}$/',
+    'TAS' => '/^7\d{3}$/',
+    'NT'  => '/^0\d{3}$/'
+];
+
+if (!preg_match($stateZips[$state], $zip)) {
+    die("Your postcode must match your state.");
+} // uses the regex rules to validate postcode based on the state chosen by the applicant
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    die("Email must be in the correct format.");
+}
+
+$phone = str_replace(' ', '', $phone); // removes spaces from the phone number
+if (!preg_match('/^\d{8,12}$/', $phone)) {
+    die("Phone number must be 8-12 characters.");
+} // checks length of phone number
 
 $query = "INSERT INTO eoi (refNum, firstName, lastName, dob, gender, address, suburb, state, zip, email, phone, skillsCPP, skillsHTML, skillsCSS, skillsJS, skillsMySQL, skillsPS, skillsGA, skillsOther) VALUES ('$refNum', '$firstName', '$lastName', '$dob', '$gender', '$address', '$suburb', '$state', '$zip', '$email', '$phone', '$skillsCPP', '$skillsHTML', '$skillsCSS', '$skillsJS', '$skillsMySQL', '$skillsPS', '$skillsGA', '$otherSkills')";
 $result = mysqli_query($conn, $query);
